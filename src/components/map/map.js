750211e20747e887866a4cadede1f47937d90a7a
Aleksandar Bogdanovic <aleksandar.bogdanovic.18@singimail.rs>
2022-02-19 21:18:48 +0100

    feat(map): add placehodler for map

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const Map = (props)=> {
 
-    return <div className="map-container">
-       <h1>This is map</h1>
-    </div>
+    return (
+    <div className="map-container">
+       <h1>This is a map page</h1>
+       <p> Placehodler for map</p>
+    </div>);
 }
 
 export default Map;
