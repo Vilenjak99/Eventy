a1db892a29d62135bd629e40360b23d5ebb8dbd9
Aleksandar Bogdanovic <aleksandar.bogdanovic.18@singimail.rs>
2022-02-19 21:16:44 +0100

    refactor(map): preetify code

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const Map = (props)=> {
 
-    return <div className="map-container">
+    return (
+    <div className="map-container">
        <h1>This is map</h1>
     </div>
+    );
 }
 
 export default Map;
